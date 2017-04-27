e2fb2cb353c56ea0c1e6d1f75b2cb21e2d3ccf48
GabrielDJB <gabrieldjb@gmail.com>
2017-04-27 18:05:43 -0400

    Update conv_utf.h
    Tirei um comentario que nao deveria estar aqui

--- conv_utf.h
+++ conv_utf.h
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-//Funcao que examina o DOM (Privada)
-//int BOM_BE (int BOM);
-
 int utf16_8(FILE* arq_entrada, FILE* arq_saida);
 
 int utf8_16(FILE* arq_entrada, FILE* arq_saida);
